c0be19e53672e8cf13443d4ff3bf979064f547b1
Remi-Debu <78204689+Remi-Debu@users.noreply.github.com>
2021-03-31 12:03:17 +0200

    Delete Ex9.3.php

--- Ex9.3.php
+++
@@ -1,9 +0,0 @@
-<?php
-$phrase = readline("Entrer votre phrase : ");
-$occurence = 1;
-for ($i = 0; $i < strlen($phrase); $i++) {
-    if ($phrase[$i] == " ") {
-        $occurence++;
-    }
-}
-echo "Le nombre de mot dans la phrase saisi est : " . $occurence;
