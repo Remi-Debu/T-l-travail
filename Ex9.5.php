86d855db5e0c2bb164ebe980f3c2779907f376be
Remi-Debu <78204689+Remi-Debu@users.noreply.github.com>
2021-03-31 12:03:27 +0200

    Delete Ex9.5.php

--- Ex9.5.php
+++
@@ -1,9 +0,0 @@
-<?php
-$phrase = readline("Entrer votre phrase : ");
-$caractereSuppr = readline("Entrer le rang du caractère à supprimer : ");
-
-for ($i = $caractereSuppr; $i < strlen($phrase) - 1; $i++) {
-    $phrase[$i] = $phrase[$i + 1];
-}
-$phrase = substr($phrase, 0, -1);
-echo "Votre nouvelle phrase : " . $phrase;
